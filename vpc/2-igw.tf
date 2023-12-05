479b487aafed7c2d8ba9ac29095934232b98f5ba
Jesus Suarez <jesus.a.suarez@gmail.com>
2023-12-05 02:34:11 +0100

    fix: version ref

--- vpc/2-igw.tf
+++ vpc/2-igw.tf
@@ -1,5 +1,5 @@
 module "igw" {
-  source = "git@github.com:jsuarezm/infra-modules.git//igw?ref=vpc-v0.0.2"
+  source = "git@github.com:jsuarezm/infra-modules.git//igw?ref=v0.0.2"
 
   vpcid = aws_vpc.this.id
   env = var.env
